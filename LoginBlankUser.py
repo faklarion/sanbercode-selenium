c84994d896135e2958c0e44add10a342dbe319c2
faklarion <faisal.hazard17@gmail.com>
2022-12-15 18:50:15 +0800

    first commit

---
+++ LoginBlankUser.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+driver.get('https://www.saucedemo.com/')
+driver.find_element(By.ID, "user-name").send_keys("")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID,"login-button").click()
